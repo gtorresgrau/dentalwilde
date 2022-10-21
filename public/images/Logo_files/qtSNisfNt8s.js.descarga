49532b0dd91fc4b2e597d474e5f7bf733b8e3783
gtorresgrau <gonzalotorresgrau@gmail.com>
2022-10-21 14:20:12 -0300

    insta

---
+++ public/images/Logo_files/qtSNisfNt8s.js.descarga
@@ -0,0 +1,3 @@
+if (self.CavalryLogger) { CavalryLogger.start_js_script(document.currentScript); }/*FB_PKG_DELIM*/
+
+__d("PolarisStoriesHighlightsRoot.entrypoint",["JSResourceForInteraction"],(function(a,b,c,d,e,f,g){"use strict";a={getPreloadProps:function(a){return{queries:{}}},root:c("JSResourceForInteraction")("PolarisStoriesHighlightsRoot.react").__setRef("PolarisStoriesHighlightsRoot.entrypoint")};g["default"]=a}),98);
--- src/App.js
+++ src/App.js
@@ -6,16 +6,17 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p> Dental Wilde 
+          <br/>
+            Website under construction
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          className="InstagramPerfil"
+          href="https://www.instagram.com/dentalwildeconsultorio/?hl=es"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Visit Our Instagram Profile
         </a>
       </header>
     </div>
